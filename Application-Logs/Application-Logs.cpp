ef96e8e1acefee163303bb7513fc71c624212afa
calvi <calvi@CALVINPAD>
2025-05-11 15:12:08 +0200

    Ajoutez des fichiers projet.

---
+++ Application-Logs/Application-Logs.cpp
@@ -0,0 +1,21 @@
+﻿// Application-Logs.cpp : définit le point d'entrée de l'application.
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "CIEL - Gestion centralisee de logs" << std::endl;
+    std::cout << "Menu" << std::endl;
+    std::cout << "Choisir une option" << std::endl;
+    std::cout << "1 - Afficher log sudo" << std::endl;
+    std::cout << "2 - Afficher et enregistrer log ssh" << std::endl;
+    std::cout << "0 - Sortir du programme" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[] "; // Affichage de l'invite de saisie
+
+    // Vous pouvez ensuite ajouter du code ici pour lire l'entrée de l'utilisateur
+    // et effectuer les actions correspondantes.
+
+    return 0;
+}
